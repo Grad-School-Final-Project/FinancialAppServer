22711c406fcee9227a40210b81febc9437773dd5
Jared <hamjared5@gmail.com>
2022-08-11 19:25:40 -0600

    hello world

---
+++ src/main/java/com/example/financialappserver/FinancialAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.financialappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialAppServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/financialappserver/FinancialAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financialappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
