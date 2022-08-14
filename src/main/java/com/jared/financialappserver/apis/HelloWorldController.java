2101a5529dc721fd6b6d9dc323f1786851461f12
Jared <hamjared5@gmail.com>
2022-08-14 12:30:43 -0600

    testing master branch protection

--- src/main/java/com/jared/financialappserver/apis/HelloWorldController.java
+++ src/main/java/com/jared/financialappserver/apis/HelloWorldController.java
@@ -2,22 +2,26 @@ package com.jared.financialappserver.apis;
 import com.jared.financialappserver.apihandlers.HelloWorldHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
     private static final Logger logger = LogManager.getLogger(HelloWorldController.class);
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String helloWorld(){
         logger.trace("Hello world api called, calling handler");
         return HelloWorldHandler.handleRequest();
     }
 
-    @RequestMapping("/twoPlusTwo")
+    @GetMapping("/twoPlusTwo")
     public int twoPlusTwo(){
         return 4;
+    }
+
+    @PutMapping("/moneyApi/convertCurrency")
+    public int convertCurrency(){
+        return 7;
     }
 
 }
